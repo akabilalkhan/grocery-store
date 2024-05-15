91ec9473bf0c778624c7d547e6655db4b984137b
akabilalkhan <akakhanbilal@gmail.com>
2024-05-15 17:17:32 -0500

    items and empty cart created

---
+++ store.js
@@ -0,0 +1,11 @@
+// Grocery Store Items
+const groceryItems = [
+  { id: 1, name: "item1", price: 1.5 },
+  { id: 2, name: "item2", price: 0.5 },
+  { id: 3, name: "item3", price: 1 },
+  { id: 4, name: "item4", price: 2 },
+  { id: 5, name: "item5", price: 1.5 },
+];
+
+// Cart the will have the added items
+let cart = [];
